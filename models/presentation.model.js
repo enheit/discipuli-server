aa570996ecbfe2100a5805583ffa8f4484e150ce
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-16 20:45:40 +0300

    Add presentation model (ORM)

---
+++ models/presentation.model.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './models.setup';
+
+import Person from './person.model';
+import Specialization from './specialization.model';
+
+const Presentation = sequelize.define('presentation', {
+  personId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'person_id',
+  },
+  specializationId: {
+    type: DataTypes.INTEGER,
+    field: 'specialization_id',
+    allowsNull: false,
+  },
+  createdBy: {
+    type: DataTypes.INTEGER,
+    field: 'created_by',
+    allowsNull: false,
+  },
+  name: {
+    type: DataTypes.VARCHAR(128),
+    field: 'name',
+    allowsNull: false,
+  },
+  description: {
+    type: DataTypes.VARCHAR(30000),
+    field: 'description',
+    allowsNull: false,
+  },
+});
+
+Specialization.belongsTo(Presentation, { foreignKey: 'specializationId' });
+Person.belongsTo(Presentation, { foreignKey: 'createdBy' });
+
+export default Presentation;
