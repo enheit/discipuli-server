2060c7582acda81275b7e97a9ffc004d4e5df8eb
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-16 20:38:01 +0300

    Add lecture presentation model (ORM)

---
+++ models/lecture-presentation.model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './models.setup';
+
+import Presentation from './presentation.model';
+
+const LecturePresentation = sequelize.define('lecturePresentation', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'id',
+  },
+  parentId: {
+    type: DataTypes.INTEGER,
+    field: 'parent_id',
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.VARCHAR(64),
+    field: 'name',
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.VARCHAR(30000),
+    field: 'description',
+    allowNull: false,
+  },
+}, {
+  tableName: 'lecture_presentation',
+});
+
+Presentation.belongsTo(LecturePresentation, { foreignKey: 'parentId' });
+
+export default LecturePresentation;
