08af4fa42c685798a583a2872ba214222e7ac430
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-16 20:27:33 +0300

    Add homework model (ORM)

---
+++ models/homework.model.js
@@ -0,0 +1,38 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './models.setup';
+
+import Person from './person.model';
+import Specialization from './specialization.model';
+
+const Homework = sequelize.define('homework', {
+  personId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'person_id',
+  },
+  specializationId: {
+    type: DataTypes.INTEGER,
+    field: 'specialization_id',
+    allowNull: false,
+  },
+  createdBy: {
+    type: DataTypes.INTEGER,
+    field: 'created_by',
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.VARCHAR(128),
+    field: 'name',
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.VARCHAR(30000),
+    field: 'description',
+    allowNull: false,
+  },
+});
+
+Specialization.belongsTo(Homework, { foreignKey: 'specializationId' });
+Person.belongsTo(Homework, { foreignKey: 'createdBy' });
+
+export default Homework;
