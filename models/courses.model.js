7cece06b4a5e12cc4c0012449921a2ab297b3c04
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-16 20:18:23 +0300

    Add courses model (ORM)

---
+++ models/courses.model.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './models.setup';
+
+import Specialization from './specialization.model';
+import Course from './course.model';
+
+const Courses = sequelize.define('subscriptions', {
+  specializationId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'specialization_id',
+  },
+  courseId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'course_id',
+  },
+});
+
+Courses.belongsTo(Specialization, { foreignKey: 'specializationId' });
+Courses.belongsTo(Course, { foreignKey: 'courseId' });
+
+export default Courses;
