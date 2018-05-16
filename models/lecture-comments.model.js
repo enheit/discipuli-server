329ba1509ed8bf7208fc639ad20c0407a2c82e7e
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-16 20:31:59 +0300

    Add lecture comments model (ORM)

---
+++ models/lecture-comments.model.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './models.setup';
+
+import Person from './person.model';
+import Lecture from './lecture.model';
+
+const LectureComments = sequelize.define('lectureComments', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    field: 'person_id',
+  },
+  lectureId: {
+    type: DataTypes.INTEGER,
+    field: 'lecture_id',
+    allowNull: false,
+  },
+  personId: {
+    type: DataTypes.INTEGER,
+    field: 'person_id',
+    allowNull: false,
+  },
+  comment: {
+    type: DataTypes.VARCHAR(1024),
+    field: 'comment',
+    allowNull: false,
+  },
+}, {
+  tableName: 'lecture_comments',
+});
+
+Lecture.belongsTo(LectureComments, { foreignKey: 'lectureId' });
+Person.belongsTo(LectureComments, { foreignKey: 'personId' });
+
+export default LectureComments;
