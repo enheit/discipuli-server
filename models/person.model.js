219f26093e49b32affeb8622cc9c714a6d6463d4
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-17 16:49:29 +0300

    Add changes to the person model (ORM)

--- models/person.model.js
+++ models/person.model.js
@@ -1,32 +1,45 @@
 import { DataTypes } from 'sequelize';
 import sequelize from './models.setup';
 
-import PersonRole from './person-role.model';
+import PersonAccount from './person-account.model';
 
 const Person = sequelize.define('person', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     field: 'id',
+    autoIncrement: true
   },
   firstName: {
     type: DataTypes.STRING(254),
     field: 'first_name',
+    allowNull: false,
+    validate: {
+      len: [2, 15],
+      notEmpty: true,
+    }
   },
   lastName: {
     type: DataTypes.STRING(1024),
     field: 'last_name',
+    allowNull: false,
+    validate: {
+      len: [2, 30],
+      notEmpty: true,
+    }
   },
   about: {
     type: DataTypes.STRING(1024),
-    field: 'about'
+    field: 'about',
   },
-  roleId: {
+  personAccountId: {
     type: DataTypes.INTEGER,
-    field: 'role_id',
+    field: 'person_account_id',
+    unique: true,
+    allowNull: false,
   }
 });
 
-Person.belongsTo(PersonRole, { foreignKey: 'roleId' });
+Person.belongsTo(PersonAccount, { foreignKey: 'personAccountId' });
 
 export default Person;
