597439b83d76847680487d783c7d90fcd9ef757b
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-17 16:49:57 +0300

    Add changes to the person account model (ORM)

--- models/person-account.model.js
+++ models/person-account.model.js
@@ -1,24 +1,41 @@
 import { DataTypes } from 'sequelize';
 import sequelize from './models.setup';
 
-import Person from './person.model';
+import PersonRole from './person-role.model';
 
-const personAccount = sequelize.define('personAccount', {
-  personId: {
+const PersonAccount = sequelize.define('personAccount', {
+  id: {
     type: DataTypes.INTEGER,
-    field: 'person_id',
+    primaryKey: true,
+    field: 'id',
+    autoIncrement: true
   },
-  email: DataTypes.STRING(254),
+  email: {
+    type: DataTypes.STRING(254),
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    }
+  },
   passwordHash: {
     type: DataTypes.STRING(1024),
-    field: 'password_hash'
+    field: 'password_hash',
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  roleId: {
+    type: DataTypes.INTEGER,
+    field: 'role_id',
+    allowNull: false,
   }
 }, {
   tableName: 'person_account',
 });
 
-personAccount.removeAttribute('id');
+PersonAccount.belongsTo(PersonRole, { foreignKey: 'roleId' });
 
-personAccount.belongsTo(Person);
-
-export default personAccount;
+export default PersonAccount;
