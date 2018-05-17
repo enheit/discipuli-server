faa3a3895cc93e94509aeeeb726648fc3f6ec3ad
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-05-17 17:03:47 +0300

    Remove import duplication in types

--- graphql/types/index.js
+++ graphql/types/index.js
@@ -4,4 +4,3 @@ export { default as PersonAccount } from './person-account';
 export { default as JWT } from './jwt-token';
 export { default as Lecture } from './lecture';
 export { default as PersonProfile } from './person-profile';
-export { default as PersonAccount } from './person-account';
