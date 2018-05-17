3ae956ae250c62899acc93403f149dfd8ccac067
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-17 16:43:39 +0300

    Add person account GraphQL type

--- graphql/types/index.js
+++ graphql/types/index.js
@@ -4,3 +4,4 @@ export { default as PersonAccount } from './person-account';
 export { default as JWT } from './jwt-token';
 export { default as Lecture } from './lecture';
 export { default as PersonProfile } from './person-profile';
+export { default as PersonAccount } from './person-account';
--- graphql/types/person-account.js
+++ graphql/types/person-account.js
@@ -7,29 +7,35 @@ import {
 
 export default new GraphQLObjectType({
   name: 'PersonAccount',
-  description: 'The account of the user',
+  description: 'The account of a user',
   fields: {
-    personId: {
+    id: {
       type: new GraphQLNonNull(GraphQLID),
-      description: 'Person\'s id',
-      resolve(account) {
-        return account.person_id;
+      description: 'The account id',
+      resolve(personAccount) {
+        return personAccount.id;
       },
     },
     email: {
       type: new GraphQLNonNull(GraphQLString),
-      description: 'The email of the user\'s account',
-      resolve(account) {
-        return account.email;
+      description: 'The email of a user\'s account',
+      resolve(personAccount) {
+        return personAccount.email;
       },
     },
-    password: {
+    passwordHash: {
       type: new GraphQLNonNull(GraphQLString),
-      description: 'The hashed password of the user\'s account',
-      resolve(account) {
-        return account.password_hash;
+      description: 'The hashed password of a user\'s account',
+      resolve(personAccount) {
+        return personAccount.passwordHash;
       },
     },
-
+    role: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The role of a user\'s account',
+      resolve(personAccount) {
+        return personAccount.role;
+      }
+    }
   },
 });
