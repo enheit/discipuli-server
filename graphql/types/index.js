39bc79759dc683eedf5193277263db9ebab65493
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-17 16:41:02 +0300

    Add person profile GraphQL type

--- graphql/types/index.js
+++ graphql/types/index.js
@@ -3,4 +3,4 @@ export { default as PersonRole } from './person-role';
 export { default as PersonAccount } from './person-account';
 export { default as JWT } from './jwt-token';
 export { default as Lecture } from './lecture';
-export { default as LectureStatus } from './lecture-status';
+export { default as PersonProfile } from './person-profile';
---
+++ graphql/types/person-profile.js
@@ -0,0 +1,55 @@
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLNonNull,
+} from 'graphql';
+
+export default new GraphQLObjectType({
+  name: 'PersonProfile',
+  description: 'The profile of a user',
+  fields: {
+    accountId: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'The account id',
+      resolve(personProfile) {
+        return personProfile.accountId;
+      },
+    },
+    firstName: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The name of a user',
+      resolve(personProfile) {
+        return personProfile.firstName;
+      }
+    },
+    lastName: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The surname of a user',
+      resolve(personProfile) {
+        return personProfile.lastName;
+      }
+    },
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The email of a user\'s account',
+      resolve(personProfile) {
+        return personProfile.email;
+      },
+    },
+    about: {
+      type: GraphQLString,
+      description: 'The short information about a user',
+      resolve(personProfile) {
+        return personProfile.about;
+      },
+    },
+    role: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The role of a user\'s account',
+      resolve(personProfile) {
+        return personProfile.role;
+      }
+    }
+  },
+});
