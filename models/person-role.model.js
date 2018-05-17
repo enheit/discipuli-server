7966650831b2e28315dd8a17858cef4709cdd6f7
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-17 16:47:11 +0300

    Add a validation metadata to the person role model (ORM)

--- models/person-role.model.js
+++ models/person-role.model.js
@@ -1,18 +1,21 @@
 import { DataTypes } from 'sequelize';
 import sequelize from './models.setup';
 
-const personRole = sequelize.define('personRole', {
+const PersonRole = sequelize.define('personRole', {
   id: {
     type: DataTypes.INTEGER,
     field: 'id',
     primaryKey: true,
+    autoIncrement: true
   },
   role: {
     type: DataTypes.STRING(1024),
-    field: 'role'
+    field: 'role',
+    unique: true,
+    allowNull: false,
   }
 }, {
   tableName: 'person_role'
 });
 
-export default personRole;
+export default PersonRole;
